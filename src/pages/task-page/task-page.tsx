4a394cf877df12d94ac7fb6339333b2f7580776a
dato shubitidze <shuubb.23@gmail.com>
2023-04-29 21:21:33 +0500

    Developing task-page

--- src/pages/task-page/task-page.tsx
+++ src/pages/task-page/task-page.tsx
@@ -1,5 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import { User } from "../registration-page/registration-page";
 import styles from "./task-page.module.scss";
+import classNames from "classnames";
 
 export default function TaskPage() {
-  return <div>blabla</div>;
+  const navigate = useNavigate();
+
+  const retData = localStorage.getItem("currentUser");
+  let user: User;
+  if (retData) user = JSON.parse(retData);
+  else {
+    navigate("/");
+    return;
+  }
+
+  return (
+    <div className={classNames(styles.pageContainer, "pageContainer")}>
+      {user.name}
+      <img src={user.imageBlob}></img>
+    </div>
+  );
 }

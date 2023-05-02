1beaf459c59be90f2f7257db2a9453bfcc3ed81e
dato shubitidze <shuubb.23@gmail.com>
2023-05-03 00:14:34 +0500

    Fixed TaskText Overflow

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,7 +1,7 @@
 import styles from "./task.module.scss";
 import DeleteForeverSvg from "../../assets/deleteForever.svg";
 import DoneSvg from "../../assets/done.svg";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import User, { TaskType } from "../../utilities/models/user";
 import classNames from "classnames";
 
@@ -15,6 +15,8 @@ interface Props {
 export default function Task(props: Props) {
   const [index] = useState(props.index);
 
+  const taskTextRef = useRef<HTMLParagraphElement>(null);
+
   function handleDelete(): void {
     props.user.deleteTask(index);
     props.reRender();
@@ -25,6 +27,42 @@ export default function Task(props: Props) {
     props.reRender();
   }
 
+  useEffect(() => {
+    function handleMouseDown(downE: MouseEvent) {
+      const initialX = downE.clientX;
+      let lastCurserPos = 0;
+      const realScrollWidth =
+        taskTextRef.current!.scrollWidth - taskTextRef.current!.clientWidth;
+
+      function handleMouseMove(moveE: MouseEvent) {
+        let curserPos = initialX - moveE.clientX;
+
+        let nextPos =
+          taskTextRef.current!.scrollLeft + curserPos - lastCurserPos;
+
+        if (nextPos >= realScrollWidth || nextPos <= 0)
+          taskTextRef.current!.style.left = `${-Math.cbrt(curserPos)}px`;
+        taskTextRef.current!.scrollLeft = nextPos;
+        lastCurserPos = curserPos;
+      }
+
+      function handleMouseUp() {
+        window.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("mouseup", handleMouseUp);
+        taskTextRef.current!.style.left = `0px`;
+      }
+
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("mouseup", handleMouseUp);
+    }
+
+    taskTextRef.current?.addEventListener("mousedown", handleMouseDown);
+
+    return () => {
+      taskTextRef.current?.removeEventListener("mousedown", handleMouseDown);
+    };
+  }, []);
+
   return (
     <div
       className={classNames(
@@ -32,11 +70,11 @@ export default function Task(props: Props) {
         props.task.isDone ? styles.gray : ""
       )}
     >
-      <p>{props.task.taskText}</p>
+      <p ref={taskTextRef}>{props.task.taskText}</p>
       <div className={styles.controlButtonsContainer}>
         <img
           src={DoneSvg}
-          className={classNames(styles.doneButton)}
+          className={styles.doneButton}
           alt={"taskDone" + props.index}
           onClick={handleDone}
         />
